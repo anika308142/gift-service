2d8e3f2da0930b99ec3df78f40eff5d68f398573
anika308142 <azrin.nahar308@gmail.com>
2024-01-25 00:19:07 +0600

    chore: change migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->unique();
             $table->string('password')->nullable();
             $table->string('designation')->nullable();
             $table->string('organization')->nullable();
